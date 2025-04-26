8058f9f9bc4a4d0277734e0e3cff47d825e645df
moohalem <mohalem.public@gmail.com>
2025-04-26 23:54:27 +0700

    add __main__

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
-from fastapi import FastAPI, File, UploadFile, HTTPException
-from fastapi.responses import JSONResponse
-from fastapi.concurrency import run_in_threadpool
-import numpy as np
-from PIL import Image
 import io
+
+import numpy as np
+import uvicorn
+from fastapi import FastAPI, File, HTTPException, UploadFile
+from fastapi.concurrency import run_in_threadpool
+from fastapi.responses import JSONResponse
+from PIL import Image
+
 from pipeline import ChillerBrandPipeline
 
 # Instantiate your pipeline
 pipeline = ChillerBrandPipeline(
     chiller_cls_model_path="weights/chiller_cls_prod.pt",
     shape_seg_model_path="weights/shape_seg_prod.pt",
-    brand_cls_model_path="weights/brand_cls_prod.pt"
+    brand_cls_model_path="weights/brand_cls_prod.pt",
 )
 
 app = FastAPI()
 
+
 def read_imagefile(file) -> np.ndarray:
-    image = Image.open(io.BytesIO(file)).convert('RGB')
+    image = Image.open(io.BytesIO(file)).convert("RGB")
     return np.array(image)
+
 
 @app.post("/detect")
 async def detect(file: UploadFile = File(...)):
@@ -32,7 +37,9 @@ async def detect(file: UploadFile = File(...)):
     if not is_chiller:
         return JSONResponse(
             status_code=400,
-            content={"detail": "Not a photo of a chiller. Please upload a correct image."}
+            content={
+                "detail": "Not a photo of a chiller. Please upload a correct image."
+            },
         )
 
     # Step 2-3: Segment + Get Cans
@@ -56,8 +63,8 @@ async def detect(file: UploadFile = File(...)):
     # Step 8: Run CanCounter
     counts = pipeline.run_counter(cans, updated_brands)
 
-    return {
-        "cans": cans,
-        "brands": updated_brands,
-        "counts": counts
-    }
+    return {"cans": cans, "brands": updated_brands, "counts": counts}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:pipeline", host="127.0.0.1", port=8000, reload=True)
